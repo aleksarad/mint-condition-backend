1b64176921c6827f5fcea049c3108408716878c4
aleksarad <alexa.radevich1@gmail.com>
2020-07-07 19:27:18 -0400

    changed json render structure

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,13 +1,13 @@
 class DaysController < ApplicationController
     def index
         days = Day.all 
-        render json: days
+        render json: days, include: "tasks.category"
     end
 
     def show
         day = Day.find(params[:id])
         if day
-            render json: day
+            render json: day, include: "tasks.category"
           else
             render json: { error: "Not found!" }, status: 404
         end
--- app/serializers/day_serializer.rb
+++ app/serializers/day_serializer.rb
@@ -1,5 +1,4 @@
 class DaySerializer < ActiveModel::Serializer
   has_many :tasks
-  has_many :categories, through: :tasks
   attributes :id, :created_at
 end
